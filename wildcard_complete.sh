e537b5504fc7f58ee0dfb4f045a3558ba732d8fc
MikJagger <55618101+MikJagger@users.noreply.github.com>
2021-02-04 15:00:26 +0100

    Update wildcard_complete.sh

--- wildcard_complete.sh
+++ wildcard_complete.sh
@@ -1,4 +1,4 @@
 cp /etc/letsencrypt/live/php4.eu/privkey.pem /opt/gophish/domain.key && cp /etc/letsencrypt/live/php4.eu/fullchain.pem /opt/gophish/domain.crt
-rn /opt/gophish/config.json /opt/gophish/config.json_bak
-cp /home/gogophish/config.sjon /opt/gophish/config.json
+mv /opt/gophish/config.json /opt/gophish/config.json_bak
+cp /home/gogophish/config.json /opt/gophish/config.json
 service gophish restart
